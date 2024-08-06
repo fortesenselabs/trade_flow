cc957668425afdeffdfeec661b9e8477653778ff
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-08-06 13:35:17 +0100

    update mt5 example

--- infrastructure/MetaTrader5/mt5_example.py
+++ infrastructure/MetaTrader5/mt5_example.py
@@ -1,5 +1,10 @@
+import os
+import time
 import pandas as pd
 from mt5linux import MetaTrader5
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # connecto to the server
 mt5 = MetaTrader5(
@@ -7,11 +12,17 @@ mt5 = MetaTrader5(
     # port = 18812       (default)
 ) 
 # # use as you learned from: https://www.mql5.com/en/docs/integration/python_metatrader5/
-mt5.initialize()
+
+# establish connection to the MetaTrader 5 terminal
+if not mt5.initialize():
+    print("initialize() failed, error code =",mt5.last_error())
+    quit()
+    
+
 mt5.login(
-   "30565290",                    # account number
-   password="@Ssc21707232",      # password
-   server="Deriv-Demo",          # server name as it is specified in the terminal
+   os.environ['MT5_ACCOUNT_NUMBER'],         # account number
+   password=os.environ['MT5_PASSWORD'],      # password
+   server=os.environ['MT5_SERVER'],          # server name as it is specified in the terminal
    timeout=120           # timeout
 )
 
@@ -23,6 +34,107 @@ rates = mt5.copy_rates_from_pos('Step Index',mt5.TIMEFRAME_M1,0,10000)
 rates_df = pd.DataFrame(rates)
 print(rates_df)
 
-mt5.shutdown()
+# prepare the buy request structure
+symbol = "Step Index"
+symbol_info = mt5.symbol_info(symbol)
+if symbol_info is None:
+    print(symbol, "not found, can not call order_check()")
+    mt5.shutdown()
+    quit()
+ 
+# if the symbol is unavailable in MarketWatch, add it
+if not symbol_info.visible:
+    print(symbol, "is not visible, trying to switch on")
+    if not mt5.symbol_select(symbol,True):
+        print("symbol_select({}}) failed, exit",symbol)
+        mt5.shutdown()
+        quit()
+ 
+lot = 0.1
+point = mt5.symbol_info(symbol).point
+price = mt5.symbol_info_tick(symbol).ask
+deviation = 20
 
+
+request = {
+    "action": mt5.TRADE_ACTION_DEAL,
+    "symbol": symbol,
+    "volume": lot,
+    "type": mt5.ORDER_TYPE_BUY,
+    "price": price,
+    "sl": price - 100 * point,
+    "tp": price + 100 * point,
+    "deviation": deviation,
+    "magic": 234000,
+    "comment": "python script open",
+    "type_time": mt5.ORDER_TIME_GTC,
+    "type_filling": mt5.ORDER_FILLING_FOK,
+}
+ 
+# send a trading request
+result = mt5.order_send(request)
+# check the execution result
+print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol,lot,price,deviation));
+if result.retcode != mt5.TRADE_RETCODE_DONE:
+    print("2. order_send failed, retcode={}".format(result.retcode))
+    # request the result as a dictionary and display it element by element
+    result_dict=result._asdict()
+    for field in result_dict.keys():
+        print("   {}={}".format(field,result_dict[field]))
+        # if this is a trading request structure, display it element by element as well
+        if field=="request":
+            traderequest_dict=result_dict[field]._asdict()
+            for tradereq_filed in traderequest_dict:
+                print("       traderequest: {}={}".format(tradereq_filed,traderequest_dict[tradereq_filed]))
+    print("shutdown() and quit")
+    mt5.shutdown()
+    quit()
+ 
+
+print("2. order_send done, ", result)
+print("   opened position with POSITION_TICKET={}".format(result.order))
+
+sleep_time=10
+print("   sleep {} seconds before closing position #{}".format(sleep_time, result.order))
+time.sleep(sleep_time)
+
+# create a close request
+position_id=result.order
+price=mt5.symbol_info_tick(symbol).bid
+deviation=20
+request={
+    "action": mt5.TRADE_ACTION_DEAL,
+    "symbol": symbol,
+    "volume": lot,
+    "type": mt5.ORDER_TYPE_SELL,
+    "position": position_id,
+    "price": price,
+    "deviation": deviation,
+    "magic": 234000,
+    "comment": "python script close",
+    "type_time": mt5.ORDER_TIME_GTC,
+    "type_filling": mt5.ORDER_FILLING_FOK,
+}
+# send a trading request
+result=mt5.order_send(request)
+
+# check the execution result
+print("3. close position #{}: sell {} {} lots at {} with deviation={} points".format(position_id,symbol,lot,price,deviation));
+if result.retcode != mt5.TRADE_RETCODE_DONE:
+    print("4. order_send failed, retcode={}".format(result.retcode))
+    print("   result",result)
+else:
+    print("4. position #{} closed, {}".format(position_id,result))
+    # request the result as a dictionary and display it element by element
+    result_dict=result._asdict()
+    for field in result_dict.keys():
+        print("   {}={}".format(field,result_dict[field]))
+        # if this is a trading request structure, display it element by element as well
+        if field=="request":
+            traderequest_dict=result_dict[field]._asdict()
+            for tradereq_filed in traderequest_dict:
+                print("       traderequest: {}={}".format(tradereq_filed,traderequest_dict[tradereq_filed]))
+ 
+# shut down connection to the MetaTrader 5 terminal
+mt5.shutdown()
 
