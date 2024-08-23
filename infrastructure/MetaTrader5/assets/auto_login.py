f091453185eaafcf127a13e86baf7f0c968d8236
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-08-23 23:57:26 +0100

    feat: enhance MetaTrader 5 container with auto-login and improved usability

--- infrastructure/MetaTrader5/assets/auto_login.py
+++ infrastructure/MetaTrader5/assets/auto_login.py
@@ -60,6 +60,8 @@ class VNCMT5Client:
         
         :param server: The broker server name to search for.
         """
+        time.sleep(5)
+
         # Click on the File tab
         self.client.mouseMove(20, 22)
         self.client.mousePress(1)
@@ -77,7 +79,7 @@ class VNCMT5Client:
 
         self.clear_and_type_value(server, empty_field=True)
         self.client.keyPress('enter')
-        time.sleep(2)
+        time.sleep(5)
 
         # Close search Dialog 
         self.client.mouseMove(930, 630)
@@ -94,7 +96,7 @@ class VNCMT5Client:
         """
         # Probe server
         self.ping_mt_server(server)
-        time.sleep(5)
+        time.sleep(2)
 
         # Click on the File tab
         self.client.mouseMove(20, 22)
@@ -123,7 +125,7 @@ class VNCMT5Client:
         # Enable Algo trading
         self.client.mouseMove(890, 50)
         self.client.mousePress(1)
-        time.sleep(0.1)
+        time.sleep(0.2)
 
     def open_journal_tab(self):
         """
@@ -132,7 +134,7 @@ class VNCMT5Client:
         # Try to open Journal tab (optional)
         self.client.mouseMove(690, 760)
         self.client.mousePress(1)
-        time.sleep(0.1)
+        time.sleep(0.2)
 
     def capture_screenshot(self, file_name='screenshot.png'):
         """
@@ -149,35 +151,108 @@ class VNCMT5Client:
         """
         Disconnects the VNC client.
         """
-        self.client.disconnect()
+        if self.client is not None:
+            self.client.disconnect()
+            self.client = None
 
+    def _set_login_successful_env_var(self):
+        """
+        Sets an environment variable to indicate a successful login.
+        """
+        os.environ['LOGIN_SUCCESSFUL'] = 'true'
+        # os.putenv('LOGIN_SUCCESSFUL', 'true')
 
-if __name__ == "__main__":
+    def verify_login(self, login, password, server):
+        """
+        Verifies if the MetaTrader 5 login was successful.
+
+        This method attempts to log in to the MetaTrader 5 server using the provided credentials.
+        If the login is unsuccessful, it raises an exception with the error details.
+
+        :param login: The MetaTrader 5 account login number.
+        :param password: The MetaTrader 5 account password.
+        :param server: The MetaTrader 5 server name.
+        :return: True if the login is successful, False otherwise.
+        :raises: Exception if the login fails, with the error code and description.
+        """
+
+        time.sleep(15)
+
+        import MetaTrader5 as mt5
+
+        # Attempt to establish a connection to the MetaTrader 5 terminal
+        is_connected = mt5.initialize(
+            "",
+            login=int(login),
+            password=password,
+            server=server,
+            timeout=120,
+            portable=True
+        )
+
+        if is_connected:
+            print(f"Login successful: {is_connected}")
+            self._set_login_successful_env_var()
+            return True
+        else:
+            error_code, error_description = mt5.last_error()
+
+            # check for IPC timeout
+            if error_code == -10005:
+                # Probe server
+                self.ping_mt_server(server)
+                time.sleep(0.5)
+                self.verify_login()
+
+            raise Exception(f"Login failed, error code = {error_code}, description = {error_description}")
+
+def main():
+    # Load environment variables
     load_dotenv()
 
+    # Retrieve MetaTrader 5 credentials from environment variables
+    login = os.getenv('MT5_ACCOUNT_NUMBER')
+    password = os.getenv('MT5_PASSWORD')
+    server = os.getenv('MT5_SERVER')
+
+    # Ensure required environment variables are set
+    if not all([login, password, server]):
+        raise ValueError("Required environment variables (MT5_ACCOUNT_NUMBER, MT5_PASSWORD, MT5_SERVER) are not set.")
+    
     VNC_SERVER_URL = "localhost"
     VNC_SERVER_PASSWORD = None
 
-    login = os.environ['MT5_ACCOUNT_NUMBER']
-    password = os.environ['MT5_PASSWORD']
-    server = os.environ['MT5_SERVER']
-
     # Create a VNCMT5Client instance
-    vnc_mt5_client = VNCMT5Client(VNC_SERVER_URL, password=VNC_SERVER_PASSWORD)
+    vnc_mt5_client = VNCMT5Client(server_url=VNC_SERVER_URL, password=VNC_SERVER_PASSWORD)
 
-    # Log in to MetaTrader 5
-    vnc_mt5_client.login_to_mt5(login, password, server)
+    try:
+        # Log in to MetaTrader 5
+        vnc_mt5_client.login_to_mt5(login, password, server)
 
-    # Enable algorithmic trading
-    vnc_mt5_client.enable_algo_trading()
+        # Enable algorithmic trading
+        vnc_mt5_client.enable_algo_trading()
 
-    # Optionally open the Journal tab
-    vnc_mt5_client.open_journal_tab()
+        # Optionally open the Journal tab
+        vnc_mt5_client.open_journal_tab()
 
-    # Capture a screenshot
-    vnc_mt5_client.capture_screenshot()
+        # Verify login
+        vnc_mt5_client.verify_login(login, password, server)
 
-    # Disconnect the VNC client
-    vnc_mt5_client.disconnect()
+        raise KeyboardInterrupt("Login attempt completed successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    print('Login Attempt Done...')
+    finally:
+        # Disconnect the VNC client
+        vnc_mt5_client.disconnect()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# 
+# There are two login approaches, 
+#  - Only once: Where the file is utilized during the container startup.
+#  - API connect: Where the file is called ass some sort of api to login the account, off course we can use the existing rpyc server.
+# https://chatgpt.com/c/46d9d791-9271-4122-b462-f248e2671e16
