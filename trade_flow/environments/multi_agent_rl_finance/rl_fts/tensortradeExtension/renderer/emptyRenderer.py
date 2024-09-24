89b755817b89a01ef88fdc832a9917f4523ce7b1
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-09-24 23:47:38 +0100

    feat: add sample multi-agent environment

---
+++ trade_flow/environments/multi_agent_rl_finance/rl_fts/tensortradeExtension/renderer/emptyRenderer.py
@@ -0,0 +1,15 @@
+from trade_flow.environments.generic import Renderer
+
+
+class EmptyRenderer(Renderer):
+    """A renderer that does renders nothing.
+
+    Needed to make sure that environment can function without requiring a
+    renderer.
+    """
+
+    def render(self, env, **kwargs):
+        pass
+
+    def close(self, _):
+        pass
