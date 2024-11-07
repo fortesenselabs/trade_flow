fa29110e77f344db4fdc8512595b394427cfc90e
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-11-07 20:21:15 +0100

    Removed environment portfolio

--- trade_flow/environments/generic/portfolio/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base import PortfolioFacade
-from .portfolio import Portfolio
--- trade_flow/environments/generic/portfolio/portfolio.py
+++
@@ -1,6 +0,0 @@
-from trade_flow.environments.generic.portfolio.base import PortfolioFacade
-
-
-class Portfolio(PortfolioFacade):
-    def __init__(self):
-        pass
