4a1ba268cab805e7dfcabb887d69bf88d9820fa1
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-12-05 08:48:16 +0100

    add logging

--- packages/mt5any/__init__.py
+++ packages/mt5any/__init__.py
@@ -5,3 +5,4 @@ Provides an API integration for Metatrader 5 with a Dockerized Terminal
 
 from .MetaTrader5 import *
 from .terminal import ContainerStatus, DockerizedMT5TerminalConfig, DockerizedMT5Terminal
+from .logging import *
